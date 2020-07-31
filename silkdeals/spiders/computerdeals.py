e1c5c86943f9c92562c0515fee0fa7d647ec2296
Binish Joshi <binishjoshi@protonmail.com>
2020-07-31 15:33:31 +0545

    More scraping

--- silkdeals/spiders/computerdeals.py
+++ silkdeals/spiders/computerdeals.py
@@ -1,10 +1,32 @@
 import scrapy
+from scrapy_selenium import SeleniumRequest
 
 
 class ComputerdealsSpider(scrapy.Spider):
     name = 'computerdeals'
-    allowed_domains = ['slickdeals.net/computer-deals']
-    start_urls = ['http://slickdeals.net/computer-deals/']
+
+    def start_requests(self):
+        yield SeleniumRequest(
+            url='https://slickdeals.net/computer-deals',
+            wait_time=3,
+            callback=self.parse
+        )
 
     def parse(self, response):
-        pass
+        products = response.xpath(
+            "//ul[@class='dealTiles categoryGridDeals']/li")
+        for product in products:
+            yield {
+                'name': product.xpath(".//a[@class='itemTitle bp-c-link']/text()").get(),
+                'link': 'https://slickdeals.net/computer-deals' + product.xpath(".//a[@class='itemTitle bp-c-link']/@href").get(),
+                'store_name': product.xpath("normalize-space(.//span[@class='blueprint']/*[1]/text())").get(),
+                'price': product.xpath("normalize-space(.//div[@class='itemPrice  wide ']/text())").get()
+            }
+        next_page = response.xpath("//a[@data-role='next-page']/@href").get()
+        if next_page:
+            absolute_url = f"https://slickdeals.net/{next_page}"
+            yield SeleniumRequest(
+                url=absolute_url,
+                wait_time=3,
+                callback=self.parse
+            )
